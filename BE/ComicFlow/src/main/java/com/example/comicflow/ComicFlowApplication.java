ad94e1c9632c8813fff611c65d500efdb989cb91
xchinh <232745627+xchinh@users.noreply.github.com>
2026-05-17 17:43:25 +0700

    init project

---
+++ BE/ComicFlow/src/main/java/com/example/comicflow/ComicFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.comicflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComicFlowApplication.class, args);
+    }
+
+}
---
+++ BE/ComicFlow/src/test/java/com/example/comicflow/ComicFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comicflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
